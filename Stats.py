6a32fd53abc8d35a05a77d43f531e71352ddb50b
Charlz John Louise C. Castillo <100450796+Spiqx@users.noreply.github.com>
2022-03-18 22:31:00 +0800

    Update Stats.py

--- Stats.py
+++ Stats.py
@@ -3,16 +3,16 @@
 #LABORATORY 2 (POST-LAB PROBLEM)
 
 
-1. Filename: stats.py
-A group of statisticians at a local college has asked you to create a set 
-of functions that compute the median and mode of a set of numbers, as defined in the below sample programs: 
-· mode.py 
-· median.py
-Define these functions in a module named stats.py. 
-Also include a function named mean, which computes the average of a set 
-of numbers. Each function should expect a list of numbers as an argument 
-and return a single number. Each function should return 0 if the list is empty. 
-Include a main function that tests the three statistical functions with a given list
+#1. Filename: stats.py
+#A group of statisticians at a local college has asked you to create a set 
+#of functions that compute the median and mode of a set of numbers, as defined in the below sample programs: 
+#· mode.py 
+#· median.py
+#Define these functions in a module named stats.py. 
+#Also include a function named mean, which computes the average of a set 
+#of numbers. Each function should expect a list of numbers as an argument 
+#and return a single number. Each function should return 0 if the list is empty. 
+#Include a main function that tests the three statistical functions with a given list
 
 def GetMode(listNum):
     
