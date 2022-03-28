57b908862801d811736e2a5d99f996d4cf466633
Genesis Gabriel B. Tongson <100451380+GenesisTongson@users.noreply.github.com>
2022-03-29 00:23:45 +0800

    Create LabReport3Project3code.py

---
+++ LabReport3Project3code.py
@@ -0,0 +1,138 @@
+import pickle
+import random
+
+class SavingsAccount:
+
+    RATE = 0.02
+
+    def __init__(self, name, pin, balance = 0.0):
+        self.name = name
+        self.pin = pin
+        self.balance = balance
+
+    def __str__(self):
+        result =  'Name:    ' + self.name + '\n' 
+        result += 'PIN:     ' + self.pin + '\n' 
+        result += 'Balance: ' + str(self.balance)
+        return result
+
+    def getBalance(self):
+        return self.balance
+
+    def getName(self):
+        return self.name
+
+    def getPin(self):
+        return self.pin
+
+    def deposit(self, amount):
+        self.balance += amount
+        return None
+
+    def withdraw(self, amount):
+        if amount < 0:
+            return "Amount must be >= 0"
+        elif self.balance < amount:
+            return "Insufficient funds"
+        else:
+            self.balance -= amount
+            return None
+
+    def computeInterest(self):
+        interest = self.balance * SavingsAccount.RATE
+        self.deposit(interest)
+        return interest
+
+    def __lt__(self, other):
+        return self.name < other.name
+                                        
+class Bank:
+
+    def __init__(self, fileName = None):
+        self.accounts = {}
+        self.fileName = fileName
+        if fileName != None:
+            fileObj = open(fileName, 'rb')
+            while True:
+                try:
+                    account = pickle.load(fileObj)
+                    self.add(account)
+                except Exception:
+                    fileObj.close()
+                    break
+
+    def __str__(self):
+        return "\n".join([str(v) for (k,v) in sorted(self.accounts.items(),key=lambda cv: cv[1].getName())])
+
+    def makeKey(self, name, pin):
+        return name + "/" + pin
+
+    def add(self, account):
+        key = self.makeKey(account.getName(), account.getPin())
+        self.accounts[key] = account
+
+    def remove(self, name, pin):
+        key = self.makeKey(name, pin)
+        return self.accounts.pop(key, None)
+
+    def get(self, name, pin):
+        key = self.makeKey(name, pin)
+        return self.accounts.get(key, None)
+
+    def computeInterest(self):
+        total = 0
+        for account in self._accounts.values():
+            total += account.computeInterest()
+        return total
+
+    def getKeys(self):
+        return []
+
+    def save(self, fileName = None):
+        if fileName != None:
+            self.fileName = fileName
+        elif self.fileName == None:
+            return
+        fileObj = open(self.fileName, 'wb')
+        for account in self.accounts.values():
+            pickle.dump(account, fileObj)
+        fileObj.close()
+       
+def createBank(numAccounts = 1):
+    names = ("Chad", "Rick", "Morty", "Yoru", "Patricia",
+             "Ken", "Ryu", "Nick")
+    bank = Bank()
+    upperPin = numAccounts + 1000
+    for pinNumber in range(1000, upperPin):
+        name = random.choice(names)
+        balance = float(random.randint(100, 1000))
+        bank.add(SavingsAccount(name, str(pinNumber), balance))
+    return bank
+
+def testAccount():
+    account = SavingsAccount("Ken", "1000", 500.00)
+    print(account)
+    print(account.deposit(100))
+    print("Expect 600:", account.getBalance())
+    print(account.deposit(-50))
+    print("Expect 600:", account.getBalance())
+    print(account.withdraw(100))
+    print("Expect 500:", account.getBalance())
+    print(account.withdraw(-50))
+    print("Expect 500:", account.getBalance())
+    print(account.withdraw(100000))
+    print("Expect 500:", account.getBalance())
+
+
+def main(number = 10, fileName = None):
+
+    testAccount()
+    print("\nACCOUNT DETAILS:\n")
+    if fileName:
+        bank = Bank(fileName)
+    else:
+        bank = createBank(number)
+        print(bank)
+
+if __name__ == "__main__":
+    main()
